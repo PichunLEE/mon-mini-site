9c87d8fe55c7f492b2e32e11c2585c4870986032
PichunLEE <pc.lee.simplon@gmail.com>
2021-11-10 10:34:56 +0100

    Created pates as components

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+const Footer =() =>{
+    return(
+        <footer className="bg-dark text-white text-center"> 
+        <p> &copy; CCI DWWM </p> 
+        </footer>
+        )
+}
+export default Footer
---
+++ src/components/Gallery.js
@@ -0,0 +1,24 @@
+
+const Gallery = () => {
+    return (  
+<section>
+      <div className="container-fluid">
+      <div className="row"> <h1>Galley </h1> </div>
+        <div className="row">
+      <div className="col-12 col-md-6">
+            <div>
+        <img className="img-fluid" src="Greece.jpg" alt="pure css slider" width="800" height="280" />
+      </div>  
+      </div>  
+      <div className="col-12">  <h5>What is Lorem Ipsum?
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h5> 
+      </div>  
+      <div>
+        <img className="img-fluid" src="https://pixabay.com/photos/cliff-sea-isle-of-skye-skye-6575844/" alt="découvrir"  />
+      </div> 
+      </div>
+      </div>
+      </section>
+      )
+}
+export default Gallery
