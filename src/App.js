19f4d881858d9624496ccededd13f384d894b202
PichunLEE <pc.lee.simplon@gmail.com>
2021-11-10 11:16:49 +0100

    proudly created routes and navigation!!!

--- src/App.js
+++ src/App.js
@@ -8,15 +8,23 @@ import Home from './components/Home.js';
 import Gallery from './components/Gallery.js'
 import Contact from './components/Contact.js';
 
+import { BrowserRouter,Routes,Route } from 'react-router-dom' ;
+
 function App() {
   return (
     <div className="App">
      
-    
+     <BrowserRouter>
      <Header />
-      <Home /> 
-      <Gallery />
-      <Contact />
+     <Routes>
+     <Route path="/" element={<Home/> }/>
+     <Route path="Gallery" element={<Gallery/> } />
+     <Route path="Contact" element={<Contact/> } />
+    
+     
+      </Routes>
+      </BrowserRouter>
+
       <Footer /> 
     </div>
   );
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -13,9 +13,7 @@ const Gallery = () => {
       <div className="col-12">  <h5>What is Lorem Ipsum?
 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h5> 
       </div>  
-      <div>
-        <img className="img-fluid" src="https://pixabay.com/photos/cliff-sea-isle-of-skye-skye-6575844/" alt="découvrir"  />
-      </div> 
+      
       </div>
       </div>
       </section>
